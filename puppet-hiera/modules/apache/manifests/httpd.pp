85d8c8788609cf5c329e499e3ab36d40e0fba73d
Mukul <smilemukul2005@gmail.com>
2017-09-09 20:40:21 +0530

    Update httpd.pp

--- puppet-hiera/modules/apache/manifests/httpd.pp
+++ puppet-hiera/modules/apache/manifests/httpd.pp
@@ -6,23 +6,23 @@ class apache (
 	}
 
   file { '/var/www/puppetweb':
-      ensure => directory,
-      owner  => apache,
-      group  => apache,
-      mode   => 750,
+     ensure => directory,
+     owner  => apache,
+     group  => apache,
+     mode   => 750,
     }
 
   file { '/var/www/puppetweb/index.html':
      ensure  => file,
      mode    => '0644',
-	 source => 'puppet:///modules/apache/index.html',
-	 require => File['/var/www/puppetweb'],
+     source => 'puppet:///modules/apache/index.html',
+     require => File['/var/www/puppetweb'],
 	}
    
   service {'httpd':
     ensure => running,
     enable => true,
-	require => Package['httpd'],
+    require => Package['httpd'],
   }
   
 }
